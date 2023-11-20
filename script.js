f034341249eecda1ab651368c0c0254937bb2f9a
DenVIII <Denis226@gmail.com>
2023-11-20 16:42:30 +0300

    Add css for grid

--- script.js
+++ script.js
@@ -6,13 +6,16 @@ createGrid()
 
 function createGrid() {
     const grid = document.createElement('div');
+    const gridWrapper = document.createElement('div');
+    gridWrapper.classList.add('grid-wrapper');
     grid.classList.add('grid');
+    grid.appendChild(gridWrapper);
 
     for (let i = 0; i < gridDimensions; i++) {
         const gridSquare = document.createElement('div');
         gridSquare.classList.add('grid-square');
 
-        grid.appendChild(gridSquare);
+        gridWrapper.appendChild(gridSquare);
     }
     document.body.appendChild(grid);
 }
